f57dcd392c66da700266370164b433ca9f46c37a
sunxen <sunxen@qq.com>
2023-09-20 17:26:22 +0800

    feat: pre publish

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import mainList from './docs'
 
@@ -36,22 +34,9 @@ mainList.forEach((item) => {
 });
 
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hello" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('hello.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		// vscode.window.showInformationMessage('Hello World from hello!');
-
+	let disposable = vscode.commands.registerCommand('docsfinder.open', () => {
 		const quickPick = vscode.window.createQuickPick();
 		quickPick.placeholder = 'Search docs';
-		// quickPick.matchOnDescription = true;
 		quickPick.items = allItems;
 		quickPick.onDidChangeSelection(selection => {
 			if (selection[0]) {
